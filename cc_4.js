23f0e8bc7fda8159326cf8262b9cb31c4a3f8302
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-02 01:05:22 -0500

    Corrected for loop in Task 2

--- cc_4.js
+++ cc_4.js
@@ -16,11 +16,13 @@ console.log(`Final amount after discount: $${finalAmount}`)
 //Declare array containing five sales figures
 let sales = [200, 2100, 75, 90, 450]
 
+//Declare totalSales variable outside of loop
+totalSales = 0;
+
 //Use for loop to access calculate total sales
-for (sale in sales) {
-    totalSales = 0;
+for (sale of sales) {
     totalSales += sale;   
 }
 
 //Log total sales value to the console
-console.log("Total Sales: $" + totalSales)
+console.log(`Total Sales: $${totalSales}`)
